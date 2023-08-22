4da6c6be34938c25b1117f075ddda442a1937b95
Dhruv <dhruvsoni285@gmail.com>
2023-08-22 16:27:19 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:ecommerce/screens/home_screeen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.system,
+      home: const HomeScreen(),
+    );
+  }
+}
